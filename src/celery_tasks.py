9e97198848e788e32f217e62265e519ef07f9eb9
kitahara-saneyuki <edwarddwarf1999@gmail.com>
2024-05-16 02:07:09 +0800

    cuda running

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -1,5 +1,7 @@
+import re
 
 from celery_app import celery_app
+from embeddings import embeddings
 from elasticsearch import Elasticsearch, helpers
 
 
@@ -23,6 +25,36 @@ client = Elasticsearch(
     task_reject_on_worker_lost=True,
 )
 def ingest_data(self, **kwargs):
+    kwargs["docs"] = list(map(lambda doc: 
+        doc | {
+            "passages":list(map(lambda sentence: {
+                "text": sentence,
+                "vector": {
+                    "predicted_value": embeddings.embed_query(sentence),
+                    "is_truncated": False,
+                    "model_id": "baai__bge-large-zh-v1.5"
+                },
+            }, [sentence for sentence in re.split('(?<=[。！？…])', doc["text"]) if len(sentence) > 0]))
+        },
+        kwargs["docs"]
+    ))
+    response = helpers.bulk(
+        client,
+        kwargs["docs"],
+        refresh=True,
+        request_timeout=60 * 10,
+    )
+
+
+@celery_app.task(
+    bind=True,
+    name="ingest_data_es",
+    autoretry_for=(),
+    default_retry_delay=30,
+    acks_late=True,
+    task_reject_on_worker_lost=True,
+)
+def ingest_data_es(self, **kwargs):
     response = helpers.bulk(
         client,
         kwargs["docs"],
